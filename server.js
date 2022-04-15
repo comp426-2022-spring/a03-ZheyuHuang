768ebeb6bf22cef25a718af88c09e342a6411940
Zheyu Huang <Zheyu.huang@outlook.com>
2022-04-15 14:17:58 -0400

    formatting

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 // Require Express.js
 const express = require("express");
-//const req = require("express/lib/request");
-//const res = require("express/lib/response");
 const app = express();
-const args = require('minimist')(process.argv.slice(2))
-const port = args.port || 5000
+
+const args = require("minimist")(process.argv.slice(2));
+const port = args.port || 5000;
 
 // Copy functions from A2
 
@@ -29,44 +28,41 @@ function coinFlips(flips) {
 }
 
 function countFlips(array) {
-
   var object = {
-    heads : 0,
-    tails : 0
-  }
+    heads: 0,
+    tails: 0,
+  };
 
   array.forEach(myFunction);
 
-  function myFunction(value, index, array){
-  
-    if (value == "heads"){
+  function myFunction(value, index, array) {
+    if (value == "heads") {
       object["heads"] += 1;
-    }else{
-      object['tails'] += 1;
+    } else {
+      object["tails"] += 1;
     }
   }
-  return (object)
+  return object;
 }
 
 function flipACoin(call1) {
-    let call = call1;
-    let flip = coinFlip();
-    let result = "";
-    if (call === flip){
-      result = 'win';
-    }else{
-      result = 'lose';
-    }
-    
-    const object2 = {call, flip, result}
-    return(object2)
+  let call = call1;
+  let flip = coinFlip();
+  let result = "";
+  if (call === flip) {
+    result = "win";
+  } else {
+    result = "lose";
   }
+
+  const object2 = { call, flip, result };
+  return object2;
+}
 
 // Start an app server
 const server = app.listen(port, () => {
   console.log("App listening on port %PORT%".replace("%PORT%", port));
 });
-
 
 // Define check endpoint
 app.get("/app/", (req, res) => {
@@ -79,27 +75,26 @@ app.get("/app/", (req, res) => {
 });
 
 app.get("/app/flip", (req, res) => {
-    var flipVar = coinFlip()
-    res.status(200).json({ 'flip' : flipVar });
-  })
+  var flipVar = coinFlip();
+  res.status(200).json({ flip: flipVar });
+});
 
 app.get("/app/flips/:number", (req, res) => {
   var flipVar2 = coinFlips(req.params.number);
-  var flipVar3 = countFlips(flipVar2)
-  res.status(200).json({ "raw": flipVar2, 'summary': flipVar3 });
+  var flipVar3 = countFlips(flipVar2);
+  res.status(200).json({ raw: flipVar2, summary: flipVar3 });
 });
 
 app.get("/app/flip/call/heads", (req, res) => {
-    res.status(200).json(flipACoin("heads"));
-  });
+  res.status(200).json(flipACoin("heads"));
+});
 
-  app.get("/app/flip/call/:tails", (req, res) => {
-    res.status(200).json(flipACoin("tails"));
-  });
+app.get("/app/flip/call/:tails", (req, res) => {
+  res.status(200).json(flipACoin("tails"));
+});
 
-  // Define default endpoint
+// Define default endpoint
 // Default response for any other request
 app.use(function (req, res) {
-    res.status(404).send("404 NOT FOUND");
-  });
-  
+  res.status(404).send("404 NOT FOUND");
+});
