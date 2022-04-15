465b84a1494558f40c86577bea3e4c1931512507
Zheyu Huang <Zheyu.huang@outlook.com>
2022-04-14 23:15:46 -0400

    update 2

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Require Express.js
-const express = require('express')
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const app = express()
 
 // Start an app server
@@ -14,13 +16,16 @@ app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
 });
 
-// // Define check endpoint
-// app.get('/app/', (req, res) => {
-//     // Respond with status 200
-//         res.statusCode = 200;
-//     // Respond with status message "OK"
-//         res.statusMessage = 'OK';
-//         res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-//         res.end(res.statusCode+ ' ' +res.statusMessage)
-//     });
-    
+// Define check endpoint
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+        res.statusCode = 200;
+    // Respond with status message "OK"
+        res.statusMessage = 'OK';
+        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' +res.statusMessage)
+    });
+
+app.get('/app/xxx',(req, res) => {
+    res.status(200).json({'message': req.params.number})
+    });
