0c2bb77afbe7650ee3d07b6e334a0ed488f0361c
Zheyu Huang <Zheyu.huang@outlook.com>
2022-04-14 23:28:22 -0400

    flip test

--- server.js
+++ server.js
@@ -1,31 +1,73 @@
 // Require Express.js
-const express = require('express');
-const req = require('express/lib/request');
-const res = require('express/lib/response');
-const app = express()
+const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const app = express();
+
+// Copy functions from A2
+
+function coinFlip() {
+  let rand = Math.floor(Math.random() * 100 + 1);
+  if (rand % 2 == 0) {
+    return "heads";
+  } else {
+    return "tails";
+  }
+}
+
+function coinFlips(flips) {
+  if (!flips) {
+    flips = 1;
+  }
+  const results = [];
+  for (let i = 0; i < flips; i++) {
+    results[i] = coinFlip();
+  }
+  return results;
+}
+
+function flipACoin(call1) {
+    let call = call1;
+    let flip = coinFlip();
+    let result = "";
+    if (call === flip){
+      result = 'win';
+    }else{
+      result = 'lose';
+    }
+    
+    const object2 = {call, flip, result}
+    return(object2)
+  }
 
 // Start an app server
-var HTTP_PORT = 3000
+var HTTP_PORT = 3000;
 const server = app.listen(HTTP_PORT, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+  console.log("App listening on port %PORT%".replace("%PORT%", HTTP_PORT));
 });
 
 // Define default endpoint
 // Default response for any other request
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
+app.use(function (req, res) {
+  res.status(404).send("404 NOT FOUND");
 });
 
 // Define check endpoint
-app.get('/app/', (req, res) => {
-    // Respond with status 200
-        res.statusCode = 200;
-    // Respond with status message "OK"
-        res.statusMessage = 'OK';
-        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-        res.end(res.statusCode+ ' ' +res.statusMessage)
-    });
+app.get("/app/", (req, res) => {
+  // Respond with status 200
+  res.statusCode = 200;
+  // Respond with status message "OK"
+  res.statusMessage = "OK";
+  res.writeHead(res.statusCode, { "Content-Type": "text/plain" });
+  res.end(res.statusCode + " " + res.statusMessage);
+});
 
-app.get('/app/xxx',(req, res) => {
-    res.status(200).json({'message': req.params.number})
-    });
+// app.get("/app/flip", (req, res) => {
+    
+//   res.status(200).json({ message: req.params.number });
+// });
+
+app.get("/app/flip", (req, res) => {
+    var flipVar = coinFlip()
+    res.status(200).json({ 'flip' : flipVar });
+  })
