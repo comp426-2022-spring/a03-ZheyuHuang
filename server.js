ef1b37b62e993e67647a2bf0b65e199a397b89a8
Zheyu Huang <Zheyu.huang@outlook.com>
2022-04-14 23:42:42 -0400

    call heads works

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get("/app/flips/:number", (req, res) => {
 app.get("/app/flip/call/:heads", (req, res) => {
     var flipObj = flipACoin(req.param.heads)
     Object.assign(flipObj, {call: "heads"})
-    res.status(200).json({ flipObj });
+    res.status(200).send(flipObj)
   });
 
   // Define default endpoint
